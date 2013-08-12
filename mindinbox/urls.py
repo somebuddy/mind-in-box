f90ef29071b89f0b968fde58b379204d43c2a0a3
Somebuddy <aleksey.drogovozov@gmail.com>
2013-08-12 14:17:57 +0300

    added static urls

--- mindinbox/urls.py
+++ mindinbox/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
@@ -13,3 +14,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns (
+        '',
+        (r'^static/(.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT}),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
