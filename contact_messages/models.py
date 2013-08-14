4f6883fcb8ba7b4bcf8961119d36d3dbcb7fd860
Somebuddy <aleksey.drogovozov@gmail.com>
2013-08-14 08:55:59 +0300

    contact_message added admin

--- contact_messages/models.py
+++ contact_messages/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Message(models.Model):
-    sender_IP = models.IPAddressField()
+    sender_IP = models.IPAddressField(blank=True, default='127.0.0.1')
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     user_name = models.CharField(max_length=255, blank=True, default='')
     user_email = models.EmailField(blank=True, default='')
--- contact_messages/views.py
+++ contact_messages/views.py
@@ -1,1 +1,15 @@
-# Create your views here.
+from django.views.generic import ListView, DetaitView
+from django.views.generic.edit import CreateView
+from .models import Message
+
+
+class MessageListView(ListView):
+    model = Message
+
+
+class MessageDetailView(DetailView):
+    model = Message
+
+
+class MessageCreateView(CreateView):
+    model = Message
