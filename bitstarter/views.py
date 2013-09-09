9a3436e06c3995ac57cc1174f7d0fc62228a58f1
Somebuddy <aleksey.drogovozov@gmail.com>
2013-09-09 12:55:03 +0300

    bitstarter: disable google analytics for dev testing

--- bitstarter/views.py
+++ bitstarter/views.py
@@ -1,7 +1,12 @@
 from django.views.generic.base import TemplateView
 from datetime import datetime
 from coinbase.models import CoinbaseWallet
+from django.conf import settings
 
 class BitstarterView(TemplateView):
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(BitstarterView, self).get_context_data(**kwargs)
+        context['debug'] = settings.DEBUG
+        return context
